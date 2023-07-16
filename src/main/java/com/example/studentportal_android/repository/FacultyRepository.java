7c0d548bbb930604d19dfab665f7d9091c01eaa1
Nyasha Masese <nyasha.masese@ecocashholdings.co.zw>
2023-07-16 13:52:16 +0200

    Create Faculty Repository

---
+++ src/main/java/com/example/studentportal_android/repository/FacultyRepository.java
@@ -0,0 +1,7 @@
+package com.example.studentportal_android.repository;
+
+import domain.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacultyRepository extends JpaRepository <Faculty, Long>{
+}

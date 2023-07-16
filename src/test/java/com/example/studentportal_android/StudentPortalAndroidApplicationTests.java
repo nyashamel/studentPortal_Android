17d7e977111f0dc307755e92dee17eca24590fef
Nyasha Masese <nyasha.masese@ecocashholdings.co.zw>
2023-07-16 11:26:41 +0200

    initial commit

---
+++ src/test/java/com/example/studentportal_android/StudentPortalAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentportal_android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPortalAndroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

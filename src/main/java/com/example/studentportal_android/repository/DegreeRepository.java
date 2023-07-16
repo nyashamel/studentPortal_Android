734a163e860bd5696118d88ceefa18edd3ab55ce
Nyasha Masese <nyasha.masese@ecocashholdings.co.zw>
2023-07-16 13:48:39 +0200

    Create Degree Repository

---
+++ src/main/java/com/example/studentportal_android/repository/DegreeRepository.java
@@ -0,0 +1,8 @@
+package com.example.studentportal_android.repository;
+
+import domain.Degree;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DegreeRepository extends JpaRepository<Degree,Long> {
+
+}

85636697487181eeaa0881a5604aa7c50c04eb4b
Nyasha Masese <nyasha.masese@ecocashholdings.co.zw>
2023-07-16 11:42:09 +0200

    Create faculty entity

--- src/main/java/domain/Faculty.java
+++ src/main/java/domain/Faculty.java
@@ -1,2 +1,25 @@
-package domain;public class Faculty {
+package domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Faculty {
+    private String facultyName;
+    private String getFacultyHead;
+    private Long degreeId;
+    private Long facultyId;
+
+    public void setFacultyId(Long facultyId) {this.facultyId = facultyId;}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "faculty_Id",nullable = false)
+    public Long getFacultyId() {return facultyId;}
 }
